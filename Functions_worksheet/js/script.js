20dccfe9ce6ba074ac5de8e1d6aa6cefe5fc1480
Etrickery <ericspfister@gmail.com>
2015-02-23 19:41:02 -0500

    type out comments and guidelines
    describe each problem, notate variables and details

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,5 +1,6 @@
 /*
 Eric Pfister
+02/23/2015
 SDI 1502
 Section 01
 Functions Worksheet
@@ -11,5 +12,63 @@ Functions Worksheet
 Activity: Functions Worksheet
 (Week 4 Activities)
 
-
+Comments! Each line
+Givens as variables
+Pass givens to function
+calculations within the function
+return the result to a result variable
+dont forget to create the result variable
+console log the result in concatenated statement
+20+commits
  */
+
+
+
+
+
+
+
+/*
+Circumference
+-------------
+Calculate the circumference of a circle.
+    Parameter(s) for function:
+        Radius of the circle
+    Return:
+        Circumference of the circle
+    Result to print to the console:
+        The circumference of the circle is X
+ */
+
+
+
+
+
+
+
+
+
+/*
+Stung!
+------
+It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal
+in a function
+    Given:
+        Victim's weight (in pounds)
+    Parameter(s) for function:
+        Victim's weight (in pounds)
+    Return:
+        Number of bee stings
+    Result to print to the console:
+        It takes X bee stings to kill this animal.
+ */
+
+
+
+
+
+
+
+
+
+
